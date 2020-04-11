fae618eb54a289fe3f0fb6565f97c78cbf43f8d7
atxrenegade <harleighabel@gmail.com>
2020-04-11 16:46:46 -0500

    create new groceryList app to practice closures

---
+++ js/main.js
@@ -0,0 +1,42 @@
+initialize(); 
+
+//*****************************************************************
+function initialize(){
+  // add event listeners to buttons
+}
+
+function displayList(){
+  // display table of grocery items
+}
+
+function addItem(name, quantity, cost){
+  //add fields for new item
+  //adds button with event listener to append new items to list
+  //invokes updateCost
+  //invokes countItem
+  //displaylist, cost and count
+}
+
+function updateCost(itemSum, operationToPerform) {
+  // update cost by adding or removing item
+}
+
+function deleteItem(item){
+  // delete item
+  // update cost
+  // display new table
+}
+
+function countItems(){
+  // total items by count and quantity
+}
+
+function resetList(){
+  //if groceryList === notdefined {
+    //add message 'List is already Empty'
+  //} else {
+    // delete list
+    // set cost to 0
+    // set items to 0
+  //}
+}
