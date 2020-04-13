e7644432fa3693f39e3406809a6135fd363b4e3d
atxrenegade <harleighabel@gmail.com>
2020-04-13 15:21:04 -0500

    add event listeners to initalize function

--- js/main.js
+++ js/main.js
@@ -3,32 +3,40 @@ initialize();
 //*****************************************************************
 function initialize(){
   // add event listeners to buttons
-}
-
-function displayList(){
-  // display table of grocery items
+  document.getElementById('btn-add-item').addEventListener('click', addItem)
+  document.getElementById('btn-del-item').addEventListener('click', deleteItem)
+  document.getElementById('btn-reset').addEventListener('click', resetList)
 }
 
 function addItem(name, quantity, cost){
-  //add fields for new item
+  //create item field with button and event listener
+  //get data from event
   //adds button with event listener to append new items to list
   //invokes updateCost
   //invokes countItem
   //displaylist, cost and count
 }
 
+function createItemField(){
+  //create fields with save button
+}
+
 function updateCost(itemSum, operationToPerform) {
   // update cost by adding or removing item
+}
+
+function countItems(){
+  // total items by count and quantity
+}
+
+function displayList(){
+  // display table of grocery items
 }
 
 function deleteItem(item){
   // delete item
   // update cost
   // display new table
-}
-
-function countItems(){
-  // total items by count and quantity
 }
 
 function resetList(){
