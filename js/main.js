d26cb2992f1f3867552cdd9f1d6b7a9389873fbc
atxrenegade <harleighabel@gmail.com>
2020-04-14 11:56:40 -0500

    build reset function, add semicolons to line endings

--- js/main.js
+++ js/main.js
@@ -3,24 +3,21 @@ initialize();
 //*****************************************************************
 function initialize(){
   // add event listeners to buttons
-  document.getElementById('btn-add-item').addEventListener('click', addItem)
-  document.getElementById('btn-del-item').addEventListener('click', deleteItem)
-  document.getElementById('btn-reset').addEventListener('click', resetList)
+  document.getElementById('btn-add-item').addEventListener('click', addItem);
+  document.getElementById('btn-del-item').addEventListener('click', deleteItem);
+  document.getElementById('btn-reset').addEventListener('click', resetList);
 }
 
 function addItem(){
   if (inputField == null) {
-    createItemField('field-add-item')
-    document.getElementById('btn-save').addEventListener('click', saveItem)
+    createItemField('field-add-item');
+    document.getElementById('btn-save').addEventListener('click', saveItem);
   } 
 
-  var inputField = document.getElementById('field-add-item')
   function saveItem(event){
-    debugger;
-    var groceryList = []
-    let name = event.target.previousElementSibling.value
-    let item = {name: name, cost: undefined, price: undefined }
-    groceryList.push(item)
+    let name = event.target.previousElementSibling.value;
+    let item = { name: name, cost: undefined, price: undefined };
+    groceryList.push(item);
     console.log(groceryList)
   }
 
@@ -33,24 +30,27 @@ function addItem(){
   //invokes updateCost
   //invokes countItem
   //displayList, cost and count
+  var inputField = document.getElementById('field-add-item');
+  var groceryList = [];
 }
 
 function createItemField(fieldType){
-  let itemField = document.createElement('input')
-  let saveButton = document.createElement('button')
-  let elToAppendTo = document.getElementById('grocery-items')
+  var itemField = document.createElement('input');
+  var saveButton = document.createElement('button');
+  var elToAppendTo = document.getElementById('grocery-items');
 
   itemField.type = 'text';
   itemField.id = fieldType;
 
-  saveButton.id = 'btn-save'
-  saveButton.innerText = 'SAVE'
+  saveButton.id = 'btn-save';
+  saveButton.innerText = 'SAVE';
 
-  elToAppendTo.appendChild(itemField)
-  elToAppendTo.appendChild(saveButton)
+  elToAppendTo.appendChild(itemField);
+  elToAppendTo.appendChild(saveButton);
 }
 
 function updateCost(itemSum, operationToPerform) {
+  
   // update cost by adding or removing item
 }
 
@@ -69,11 +69,8 @@ function deleteItem(item){
 }
 
 function resetList(){
-  //if groceryList === notdefined {
-    //add message 'List is already Empty'
-  //} else {
-    // delete list
-    // set cost to 0
-    // set items to 0
-  //}
+  document.getElementById('grocery-items').innerHTML = '<br><br>'; 
+  groceryList = [];
+  cost = 0;
+  numOfItems = 0;
 }
