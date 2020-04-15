a784bbc9609acb247197150e511d7a9e281a3e7a
atxrenegade <harleighabel@gmail.com>
2020-04-15 15:01:57 -0500

    move item specfic functions into addItem(), build createCheckbox()

--- js/main.js
+++ js/main.js
@@ -18,45 +18,6 @@ function buildTable(){
   groceryTable.appendChild(groceryBody)
 }
 
-function addItem(){
-  var inputField = document.getElementById('field-add-item');
-  var groceryList = [];
-
-  if (inputField == null) {
-    createItemField('field-add-item');
-    document.getElementById('btn-save').addEventListener('click', saveItem);
-  } 
-
-  function saveItem(event){
-    var name = event.target.previousElementSibling.value;
-    event.target.previousElementSibling.value = '';
-    var item = { name: name, cost: undefined, price: undefined };
-    groceryList.push(item);
-    displayItem(item, groceryList);
-    updateTable(groceryList, 'add');
-  }
-
-  function displayItem(item, groceryList){  
-    console.log(groceryList);
-    var table = document.querySelector('table');
-    var row = table.insertRow();
-    var itemData = Object.values(item);
-    debugger;
-    itemData.forEach(el => {
-      let cell = row.insertCell();
-      let text = document.createTextNode(el);
-      cell.appendChild(text);
-    })   
-  }
-  //name, quantity, cost
-  //displayList, cost and count
-}
-
-function updateTable(groceryList, addOrDelete){
-  countItems(groceryList);
-  updateCost(groceryList, addOrDelete);
-}
-
 function createItemField(fieldType){
   var itemField = document.createElement('input');
   var saveButton = document.createElement('button');
@@ -72,17 +33,72 @@ function createItemField(fieldType){
   elToAppendTo.appendChild(saveButton);
 }
 
-function updateCost(groceryList, operationToPerform) {
+function addItem(){
+  var inputField = document.getElementById('field-add-item');
+  var groceryList = [];
+  if (inputField == null) {
+    createItemField('field-add-item');
+    document.getElementById('btn-save').addEventListener('click', saveItem);
+  } 
+
+  /***************************/
+  function saveItem(event){
+    var name = event.target.previousElementSibling.value;
+    event.target.previousElementSibling.value = '';
+    var item = { name: name, cost: undefined, price: undefined };
+    groceryList.push(item);
+    displayItem(item, groceryList);
+    updateTable(groceryList, 'add');
+  }
+
+  function displayItem(item, groceryList){  
+    console.log(groceryList);
+    var table = document.querySelector('table');
+    var row = table.insertRow();
+    createCheckbox(item, row);
+    createCellData(item, row);
+  }  
+
+  function createCheckbox(item, row){
+    debugger;
+    var itemCheckbox = document.createElement('input');
+    var cell = row.insertCell();
+    itemCheckbox.type = 'checkbox';
+    itemCheckbox.id = item.name;
+    itemCheckbox.classList.add('ckbx-styled');
+    cell.appendChild(itemCheckbox);
+    itemCheckbox.addEventListener('change', function(){ 
+      if (this.checked){ 
+        updateCost(item, 'add') 
+      } 
+    })
+  }
+
+  function createCellData(item, row){
+    var itemData = Object.values(item);
+    itemData.forEach(el => {
+      let cell = row.insertCell();
+      let text = document.createTextNode(el);
+      cell.appendChild(text);
+    })   
+  } 
+}
+
+function updateTable(groceryList, operation){
+  countItems(groceryList);
+  updateCost(groceryList, operation);
+}
+
+function updateCost(item, operationToPerform){
   // update cost by adding or removing item
 }
 
 function countItems(groceryList){
-  // create clossure to access groceryList without passing as parameter
+  // create closure to access groceryList without passing as parameter
   var count = groceryList.length;
   document.getElementById('items-num').innerText = count;
   // total items by count and quantity
 }
-
 
 function deleteItem(item){
   // delete item
